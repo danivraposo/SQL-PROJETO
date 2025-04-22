2356faf3477502e239534db8924dc276cbaed478
ViriaToRaPoso <filipedani772@gmail.com>
2025-04-22 22:22:19 +0100

    A

--- Codigo .SQL/ColocarDadosNasTabelasAssociativas.sql
+++ Codigo .SQL/ColocarDadosNasTabelasAssociativas.sql
@@ -1,5 +1,4 @@
-﻿/*
-INSERT INTO Pedido_Peca (ID_Trabalho, ID_Peca)
+﻿INSERT INTO Pedido_Peca (ID_Trabalho, ID_Peca)
 VALUES 
 (1, 1), -- Pedido 1 tem a peça 1 (Calças)
 (1, 2), -- Pedido 1 também tem a camisa
@@ -15,4 +14,3 @@ INSERT INTO Material_Encomenda (ID_Encomenda, ID_Material, Quantidade)
 VALUES 
 (1, 1, 50), -- Encomenda 1: 50 unidades de Linha branca
 (1, 2, 20); -- Encomenda 1: 20 botões metálicos
-*/
